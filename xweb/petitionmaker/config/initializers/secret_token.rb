84240e0b0151d95fabf21b211615179a4f698214
treamous <treamous@gmail.com>
2011-02-24 07:31:01 -0700

    .../login/new and .../user are working

---
+++ xweb/petitionmaker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petitionmaker::Application.config.secret_token = '3b4ea350ec77db1177371823500de296c7dca0bd540ee150afda2dbe7805576385691a997a8a4269dad1d4c766acc4a1c1b7c73ed5b52b47929ea7149c228b84'
