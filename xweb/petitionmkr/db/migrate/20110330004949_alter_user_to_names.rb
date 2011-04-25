1801a02a37800b07155e1f409725b68b964e5d7f
unknown <Srikesh@.(none)>
2011-04-24 20:09:35 -0700

    fixed a bug in db migrate

--- xweb/petitionmkr/db/migrate/20110330004949_alter_user_to_names.rb
+++ xweb/petitionmkr/db/migrate/20110330004949_alter_user_to_names.rb
@@ -3,13 +3,13 @@ class AlterUserToNames < ActiveRecord::Migration
     add_column("users", "first_name", :string, :limit => 40, :default => "")
     add_column("users", "last_name", :string, :limit => 40, :default => "")
     remove_index("users", "name")
-    remove_column("users", "name", :string, :limit => 40)
+    remove_column("users", "name", :string, :limit => 50)
     add_index("users", "username")
   end
 
   def self.down
     remove_index("users", "username")
-    add_column("users", "name", :string, :limit => 40)
+    add_column("users", "name", :string, :limit => 50)
     add_index("users", "name")    
     remove_column("users", "last_name", :string, :limit => 40, :default => "")
     remove_column("users", "first_name", :string, :limit => 40, :default => "")
