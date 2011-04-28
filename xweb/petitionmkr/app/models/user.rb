f9fdbf98504be5866dcbae90053d5aaaec7f390d
unknown <Srikesh@.(none)>
2011-04-27 19:30:45 -0700

    fixes for the bugs raised by QA team

--- xweb/petitionmkr/app/models/user.rb
+++ xweb/petitionmkr/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   
   
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  NAME_REGEX = /^[a-zA-Z]+$/
+  NAME_REGEX = /^[a-zA-Z ]+$/
   
   #validations galore...    
   validates :first_name, :presence => true, :length => { :maximum => 25 }, :format => {:with => NAME_REGEX, :message => ' should have alphabets only.'}
